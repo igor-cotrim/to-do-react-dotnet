11dc5d115dd82a1ed85f1ba3e34c75702586162e
Igor Cotrim <igor.santos@verity.com.br>
2023-01-05 14:42:19 -0300

    feat: add bootstrap and first project styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="mt-3">
+      <ul className="list-group">
+        <li className="list-group-item">Atividad 1</li>
+        <li className="list-group-item">Atividad 2</li>
+        <li className="list-group-item">Atividad 3</li>
+        <li className="list-group-item">Atividad 4</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
